593d40c020f9938ae036fa74c241ca5d663e7031
JiangMr <JiangMr@163.com>
2019-07-01 15:39:47 +0800

    ssm项目基本模板搭建dao

---
+++ day01_ssm_dao/src/main/java/com/gree/ssm/dao/IProductDao.java
@@ -0,0 +1,12 @@
+package com.gree.ssm.dao;
+
+import com.gree.ssm.domain.Product;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface IProductDao {
+     //查询所有的产品信息
+    @Select("select * from product")
+   public List<Product> findAll()throws Exception;
+}
